9b5479621e4ace69d7289fb6ab8d28f073cdbbfb
ZandrexQX <zandrex@mail.ru>
2023-02-06 11:06:56 +0300

    Task 25

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+Console.WriteLine("Task 25");
+Console.WriteLine("------");
+
+Console.Write("Введите число A:");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B:");
+int numB = Convert.ToInt32(Console.ReadLine()), result = 1;
+for (int i = 0; i < numB; i++){
+    result *= numA;
+}
+Console.WriteLine($"{numA} в степени {numB} равно {result}");
+
+Console.ReadLine();
+
+/*--------------------------------------------*/
